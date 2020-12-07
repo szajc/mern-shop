748f33e7e07ff2600d75a3da2498e8c615d18f53
szajc <szajc2000@gmail.com>
2020-12-07 20:21:49 +0100

    push to heroku #9

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoutes = require("./routes/userRouter")
@@ -8,10 +8,7 @@ const cartRoutes = require("./routes/cartRouter")
 const newsRoutes = require("./routes/newsRouter")
 const path = require('path');
 
-
-
 // set up express
-
 const app = express();
 app.use(express.json());
 app.use(cors());
