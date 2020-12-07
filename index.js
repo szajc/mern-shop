6bcc55ab53a7aa69f51ad523e3db36f6027ac619
szajc <szajc2000@gmail.com>
2020-12-07 19:06:40 +0100

    push to heroku #7

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ app.use("/cart", cartRoutes);
 app.use("/news", newsRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'frontend/build' ));
+    app.use(express.static(path.join(__dirname, 'frontend/build' )));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html' )); // relative path 
+        res.sendFile(path.join(__dirname + 'frontend/build/index.html' )); // relative path 
     })
 }
 
