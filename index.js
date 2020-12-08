2ad57f9efe5c237a342da15c406f3d814376d4da
szajc <szajc2000@gmail.com>
2020-12-08 13:27:03 +0100

    push to heroku #9

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'frontend/build' )));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + 'frontend/build/index.html' )); // relative path 
+        res.sendFile(path.join(__dirname, './frontend/build/index.html' )); // relative path 
     })
 }
 
